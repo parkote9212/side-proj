2a4b9aabc77c28764a647e96077050c7289ad123
parkote9212 <parkote9212@gmail.com>
2025-11-07 11:51:25 +0900

    feat: Spring Boot 프로젝트 초기 설정
    Spring Initializr를 사용하여 'My Onbid Dashboard' 백엔드 프로젝트의 기본 뼈대를 생성합니다.
    
    - Java 21, Spring Boot 3.5.7 환경
    - 주요 초기 의존성 추가:
      - Spring Web
      - MyBatis Framework
      - MariaDB Driver
      - Lombok
    -  설정:
      - MariaDB Datasource 연결 정보
      - MyBatis 맵퍼 경로 및

---
+++ src/main/java/com/pgc/sideproj/SideProjApplication.java
@@ -0,0 +1,13 @@
+package com.pgc.sideproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideProjApplication.class, args);
+	}
+
+}
